bba3126dbdc5fcef505a50979f9a7f56bd3cdc65
whtsky <whtsky@gmail.com>
2016-01-28 18:52:14 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from setuptools import setup
 
 setup(
-    ext_modules=cythonize("bencoder.pyx")
+    name='Maga',
+    version='1.0.0',
+    description='A DHT crawler framework using asyncio.',
+    long_description=open('README.rst', 'r').read(),
+    author='whtsky',
+    author_email='whtsky@gmail.com',
+    url='https://github.com/whtsky/maga',
+    license='BSDv3',
+    platforms='any',
+    zip_safe=False,
+    keywords=['dht', 'asyncio', 'crawler'],
+    classifiers=[
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Cython',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
 )
